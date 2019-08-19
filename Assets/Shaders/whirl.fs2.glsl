57a19088bb8a6ac006d9e660e6823de1d713300d
RPKQ <rpkq0512@gmail.com>
2019-08-19 14:37:25 +0800

    (shader) whirl updated
    change to my coding style

--- Assets/Shaders/whirl.fs2.glsl
+++ Assets/Shaders/whirl.fs2.glsl
@@ -12,30 +12,31 @@ uniform float windowW;
 uniform float windowH;
 uniform vec2 center;
 
-
-vec4 PostFX(sampler2D tex, vec2 uv)
-{
-	const float radius = 200.0;
-	const float angle = 0.8;
-	vec2 texSize = vec2(windowW, windowH);
-	vec2 tc = uv * texSize;
-	tc -= center;
-	float dist = length(tc);
-	if (dist < radius) 
-	{
-	float percent = (radius - dist) / radius;
-	float theta = percent * percent * angle * 8.0;
-	float s = sin(theta);
-	float c = cos(theta);
-	tc = vec2(dot(tc, vec2(c, -s)), dot(tc, vec2(s, c)));
-	}
-	tc += center;
-	vec3 color = texture2D(tex, tc / texSize).rgb;
-	return vec4(color, 1.0);
-}
-
 void main (void)
 {
-  vec2 uv = vertexData.texCoord;
-  gl_FragColor = PostFX(tex, uv);
+	const float radius = 200.0;
+	const float scale = 10.0;
+	vec2 texSize = vec2(windowW, windowH);
+	vec2 curPoint = vertexData.texCoord * texSize;
+	vec2 curVec = curPoint - center;
+	float dist = length(curVec);
+	if (dist < radius) 
+	{
+		// get rotate magnitude 
+		float percent = (radius - dist) / radius;
+		float rotateAngle = percent * percent * scale;
+
+		// rotate vector
+		float s = sin(rotateAngle);
+		float c = cos(rotateAngle);
+		vec2 newVec = vec2(dot(curVec, vec2(c, -s)), dot(curVec, vec2(s, c)));
+
+		// sample the texture
+		fragColor = texture(tex, (newVec + center) / texSize);
+	}
+	else
+	{
+		fragColor = texture(tex, vertexData.texCoord);
+	}
+	
 }
